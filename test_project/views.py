77783249617a62c731d26d65ad1efaeadeec277c
ebarrera2019263 <ebarrera-2019263@kinal.edu.gt>
2025-04-03 23:34:26 -0600

    se agrego cerrar sesion

--- test_project/views.py
+++ test_project/views.py
@@ -1,20 +1,20 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
-from django.contrib.auth.views import LoginView, LogoutView 
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 
 
 class IndexView(LoginRequiredMixin, View):
     @staticmethod
     def get(request):
         return redirect('spending_control:spending-list')
-    
+
 
 class SignInView(LoginView):
     template_name = 'login.html'
     success_url = reverse_lazy('spending_control:spending-list')
-    
-    
+
+
 class SignOutView(LogoutView):
-    ...
+    next_page = reverse_lazy('login')  #  Redirige al login después de logout
